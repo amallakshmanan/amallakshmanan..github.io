a46c41e262767c158f89d0e0d1d7ea13314005e0
Amal <amallaksh100@gmail.com>
2021-10-18 19:47:55 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+function showmenu() {
+    var element = document.getElementById("menu-toggle-id");
+    element.classList.toggle("show-menu");
+    var element = document.getElementById("menu-open-id");
+    element.classList.toggle("menu-close");
+    var element = document.getElementById("mainmenu-open");
+    element.classList.toggle("mainmenu-open");
+    var element = document.getElementById("logo");
+    element.classList.toggle("logo-menu");
+
+
+
+
+}
